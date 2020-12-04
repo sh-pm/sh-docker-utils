98c680aaf354d4b7d9ec9e755646111a1d161a72
sh-pm <shpmmail@gmail.com>
2020-12-04 08:46:44 -0300

    - Update sh-commons to v2.2.1

--- pom.sh
+++ pom.sh
@@ -11,7 +11,7 @@ declare -A DEPENDENCIES=( \
     [sh-pm]=v3.3.0
     [sh-logger]=v1.4.0 \
     [sh-unit]=v1.5.4 \
-	[sh-commons]=v2.0.0 \
+	[sh-commons]=v2.2.1 \
 );
 
 SSO_API_AUTHENTICATION_URL=https://shpmcenter.com/sso/rest/api/sso/authentication
