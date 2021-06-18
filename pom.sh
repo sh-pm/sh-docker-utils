a3d68d667b9caa0a85b0a41d01baff32cda4c18e
sh-pm <shpmmail@gmail.com>
2021-06-18 08:50:55 -0300

    v1.4.0
    - Update sh-pm

--- pom.sh
+++ pom.sh
@@ -3,8 +3,8 @@ ARTIFACT_ID=sh-docker-utils
 VERSION=v1.4.0
 
 declare -A DEPENDENCIES=( \
-    [sh-pm]=v4.0.0
-    [sh-logger]=v1.4.0 \
-    [sh-unit]=v1.5.4 \
-	[sh-commons]=v2.2.3 \
+    [sh-pm]=v4.0.0@github.com/sh-pm \
+    [sh-logger]=v1.4.0@github.com/sh-pm \
+    [sh-unit]=v1.5.4@github.com/sh-pm \
+	[sh-commons]=v2.2.3@github.com/sh-pm \
 );
