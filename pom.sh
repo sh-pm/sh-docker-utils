be59d82ee9cf9fc198f387037888aead91545230
sh-pm <shpmmail@gmail.com>
2021-06-18 08:49:24 -0300

    v1.4.0
    - Update sh-pm

--- pom.sh
+++ pom.sh
@@ -2,16 +2,9 @@ GROUP_ID=bash
 ARTIFACT_ID=sh-docker-utils
 VERSION=v1.4.0
 
-declare -A REPOSITORY=( \
-	[host]="shpmcenter.com" \
-	[port]=443 \
-);
-
 declare -A DEPENDENCIES=( \
-    [sh-pm]=v3.3.0
+    [sh-pm]=v4.0.0
     [sh-logger]=v1.4.0 \
     [sh-unit]=v1.5.4 \
-	[sh-commons]=v2.2.2 \
+	[sh-commons]=v2.2.3 \
 );
-
-SSO_API_AUTHENTICATION_URL=https://shpmcenter.com/sso/rest/api/sso/authentication
